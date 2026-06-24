dab711f202e3fae2964feb13deadb0dc4e7609d6
VintellX <vin@vintellx.com>
2026-06-24 12:39:05 +0530

    feat(drafts): scaffold drafts module

---
+++ usr/lib/vinmail/drafts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# VinMail
+# "Bash-ing out an email."
+
+DRAFTS_DIR="$VINMAIL_DIR/drafts"
+
+# ----- Initialize Drafts Directory -----
+initDrafts() {
+    mkdir -p "$DRAFTS_DIR"
+}
+
+# ----- Drafts Count -----
+draftCount() {}
+
+# ----- Save Draft -----
+saveDraft() {}
+
+# ----- Load Draft -----
+loadDraft() {}
+
+# ----- Delete Draft -----
+deleteDraft() {}
+
+# ----- Show Drafts -----
+showDrafts() {}
